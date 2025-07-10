1404e6cd8b0de9e653f9bfd3d978eb6ec29b9dfc
MVKARTHIK2712 <karnamuraboina@gmail.com>
2025-07-10 21:48:13 +0530

    exploring get,post,delete and patch

--- src/app.js
+++ src/app.js
@@ -2,15 +2,29 @@ const express=require("express")
 
 
 const app=express();
-app.use("/",(req,res)=>{
-    res.send("hello kk its 3000")
+
+//this will only handle get calls to /user
+app.get("/user",(req,res)=>{
+    res.send({firstname:"karna",lastname:"yadav"});
 });
+
+app.post("/user",(req,res)=>{
+    res.send("data successfully saved to the database");
+});
+
+app.delete("/user",(req,res)=>{
+    res.send("data successfully deleted");
+});
+app.patch("/user",(req,res)=>{
+    res.send("data updated successfully");
+});
+
+//this will match all the http methods api calls to /hello
 app.use("/hello",(req,res)=>{
     res.send("hello karna")
 });
-app.use("/test",(req,res)=>{
-    res.send("hello from the expreess server  ")
-});
+
+
 app.listen(3000,()=>{
     console.log("server is peerfectly listining on port 7777")
 });
