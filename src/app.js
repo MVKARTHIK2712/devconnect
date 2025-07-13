57cf02f92feedd3e00268f3cee64357b010a65ba
MVKARTHIK2712 <karnamuraboina@gmail.com>
2025-07-13 10:46:38 +0530

    wildcard-error-handling

--- src/app.js
+++ src/app.js
@@ -1,27 +1,30 @@
 const express=require("express");
 
 const app=express();
-const {aadminAuth,userAuth}=require("./middleware/auth");
 
-// handle auth middleware for all GET,POST.. requests
-app.use("/admin",aadminAuth);
-// app.use("/user",userAuth);
-
-app.get("/user/login",(req,res)=>{
-    res.send("user login is successful");
-});
-app.get("/user/data",userAuth,(req,res)=>{
-    res.send("user data is sent");
-});
-app.get("/admin/getalldata",(req,res)=>{
-    res.send("all data is sent");
-});
-app.get("/admin/deleteuser",(req,res)=>{
-    res.send("deleted a user");
+app.use("/",(err,req,res,next)=>{
+    if(err){
+    res.status(500).send("Something broke!");
+    }
+    
 });
 
+app.get("/getuserdata",(req,res)=>{
+    // try{
+        throw new Error("this is an error");
+        res.send("user data is sent");
+    // }catch(err){
+    //         res.status(500).send("Some error occurred conatact support team");
+    // }
+    
+});
 
-
+app.use("/",(err,req,res,next)=>{
+    if(err){
+    res.status(500).send("Something broke!");
+    }
+    
+});
 
 app.listen(3000,()=>{
     console.log("server is peerfectly listining on port 7777")
