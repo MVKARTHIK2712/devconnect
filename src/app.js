4d74f2d59b110f24e2adc3abfd4179ffafa9b0be
MVKARTHIK2712 <karnamuraboina@gmail.com>
2025-07-17 05:06:12 +0530

    init-database

--- src/app.js
+++ src/app.js
@@ -1,31 +1,36 @@
 const express=require("express");
-
+const connectdb=require("./config/database");
 const app=express();
+const User=require("./models/user")
 
-app.use("/",(err,req,res,next)=>{
-    if(err){
-    res.status(500).send("Something broke!");
+
+app.post("/signup", async (req, res) => {
+    // Creating new instance for user model
+    try {
+        const user = new User({
+            firstName: "umesh",
+            lastName: "yadav",
+            emailId: "umesh@gmail.com",
+            password: "umesh207",
+        });
+        await user.save();
+        res.status(201).send("User data added");
+    } catch (err) {
+        console.error("User creation failed:", err);
+        res.status(500).send("Internal server error");
     }
-    
 });
 
-app.get("/getuserdata",(req,res)=>{
-    // try{
-        throw new Error("this is an error");
-        res.send("user data is sent");
-    // }catch(err){
-    //         res.status(500).send("Some error occurred conatact support team");
-    // }
-    
-});
 
-app.use("/",(err,req,res,next)=>{
-    if(err){
-    res.status(500).send("Something broke!");
-    }
-    
-});
 
-app.listen(3000,()=>{
-    console.log("server is peerfectly listining on port 7777")
+connectdb()
+    .then(() => {
+        console.log("Database connection established");
+        app.listen(3000,()=>{
+        console.log("server is peerfectly listining on port 3000")
 });
+    })
+    .catch((err) => {
+        console.error("Database cannot be connected");
+    });
+
---
+++ src/config/database.js
@@ -0,0 +1,9 @@
+const mongoose=require('mongoose');
+
+const connectdb=async()=>{
+    mongoose.connect(
+    "mongodb+srv://karnamuraboina:sW2vOxnCK6ftv0Qz@devconnect.eappave.mongodb.net/devconnect"
+    );
+};
+
+module.exports=connectdb;
